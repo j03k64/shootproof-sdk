f0cbbd6dec8066010c45a80bff2d25e2c04d62d3
Jason Pirkey <jpirkey@kiera.local>
2014-03-30 18:13:56 -0300

    Initial commit from ShootProof SDK 1.4

---
+++ Sp_Auth.php
@@ -0,0 +1,128 @@
+<?php
+require_once('Sp_Lib.php');
+/*
+ * OAuth 2.0 authentication wrapper for authenticating with the ShootProof.com API
+ *
+ * To find more information and for documentation of the API please
+ * visit http://developer.shootproof.com
+ */
+class Sp_Auth extends Sp_Lib
+{
+    /**
+     * Property for the API authentication endpoint
+     *
+     * @var string
+     */
+    protected $_authEndPoint = 'https://auth.shootproof.com/oauth2/authorization/new';
+
+    /**
+     * Property for the API authentication token endpoint
+     *
+     * @var string
+     */
+    protected $_tokenEndPoint = 'https://auth.shootproof.com/oauth2/authorization/token';
+
+    /**
+     * Preoperty for the redirect URI
+     *
+     * @var string
+     */
+    protected $_redirectUri = 'path to file/complete.php';
+
+    /*
+     * Property for scope
+     * Full API scope shown
+     *
+     * @var string
+     */
+    protected $_scope = "sp.studio.info sp.event.get_list sp.event.create sp.event.photo_exists sp.event.get_photos sp.album.get_list sp.album.create sp.album.get_photos sp.photo.upload sp.photo.delete sp.photo.update sp.order.get_list sp.order.get_details sp.album.move sp.album.rename sp.album.delete sp.event.rename";
+
+    /**
+     * Property to hold the appId / Client Id
+     *
+     * @var string
+     */
+    protected $_clientId = 'XXXXX';
+
+	/**
+	 * Method to get the appId
+	 *
+	 * @return string
+	 */
+	protected function _getClientId()
+	{
+		return $this->_clientId;
+	}
+
+	/**
+	 * Method to return the redirect uri
+	 *
+	 * @return string
+	 */
+	protected function _getRedirectUri()
+	{
+		return $this->_redirectUri;
+    }
+
+	/**
+	 * Method to return the scope
+	 *
+	 * @return string
+	 */
+	protected function _getScope()
+	{
+		return $this->_scope;
+    }
+
+    /**
+     * Method to get the login uri
+     *
+     * @return string
+     */
+    public function getLoginUri()
+    {
+        $params = array(
+            'response_type' => 'code',
+            'client_id' => $this->_getClientId(),
+            'redirect_uri' => $this->_getRedirectUri(),
+            'scope' => $this->_getScope()
+        );
+
+        return $this->_getUri($this->_authEndPoint, $params);
+    }
+
+    /**
+    * Method to request an access token with a temporary auth code
+    *
+    * @param $code auth code
+    * @return json response with access/refresh token
+    */
+    public function requestAccessToken($code)
+    {
+        $params = array(
+            'code' => $code,
+            'grant_type' => 'authorization_code',
+            'client_id' => $this->_getClientId(),
+            'redirect_uri' => $this->_getRedirectUri(),
+            'scope' => $this->_getScope()
+        );
+
+        return $this->_makeRequest($this->_tokenEndPoint, $params);
+    }
+
+    /**
+    * Method to request an access token with a temporary auth code
+    *
+    * @param $refreshToken refresh_token
+    * @return json response with access/refresh token
+    */
+    public function requestAccessTokenRefresh($refreshToken)
+    {
+        $params = array(
+            'refresh_token' => $refreshToken,
+            'grant_type' => 'refresh_token'
+        );
+
+        return $this->_makeRequest($this->_tokenEndPoint, $params);
+    }
+}
